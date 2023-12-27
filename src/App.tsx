863119687cef35b1600aa6b480d10c63914d03e7
Roni Sarkar <ronichandrasarkar@gmail.com>
2023-12-27 13:57:42 +0530

    done simple redux project

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
+import {
+  decrement,
+  increment,
+  incrementByAmount,
+} from "./redux/features/counter/counterSlice";
+import { useAppDispatch, useAppSelector } from "./redux/hook";
+
 function App() {
+  const count = useAppSelector((state) => state.counter.value);
+  const dispatch = useAppDispatch();
+
   return (
-    <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-      Click me
-    </button>
+    <div>
+      <button
+        onClick={() => dispatch(increment())}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Increment
+      </button>
+      <br />
+      {count < 0 ? <span>0</span> : <span>{count}</span>}
+
+      <br />
+      <button
+        onClick={() => dispatch(decrement())}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Decrement
+      </button>
+      <br />
+      <br />
+      <button
+        onClick={() => dispatch(incrementByAmount(5))}
+        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      >
+        Increment By 5
+      </button>
+    </div>
   );
 }
 
